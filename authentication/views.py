d01aa498e989d21d4ed6c3205f62896a04c2232a
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-10-06 23:58:35 +0530

    Create basic login view in Python

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.contrib import messages
 
-from . import forms
+from . import forms, oktaapi
 
 
+def login(request):
+    if request.method == 'POST':
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            session = oktaapi.login(data['username'], data['password'])
+
+            if not session:
+                messages.error(request, 'Authentication was unsuccessful!')
+                request.session['sessionToken'] = session
